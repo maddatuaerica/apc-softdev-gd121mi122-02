85dc7be398a0dcaba379a033df5de23f67a25267
Aileen Mendoza <mendozaaileen61@gmail.com>
2015-07-21 14:30:09 -0700

    created myaddress and mycomment site

---
+++ Practice/midterm_test/Mendoza_Aileen_midterms/ourspace/models/Mycomment.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "mycomment".
+ *
+ * @property integer $id
+ * @property integer $myaddress_id
+ * @property string $author
+ * @property string $body
+ * @property string $created_at
+ */
+class Mycomment extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'mycomment';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['mycomment_id', 'author', 'body'], 'required'],
+            [['myaddress_id'], 'integer'],
+            [['body'], 'string'],
+            [['created_at'], 'safe'],
+            [['author'], 'string', 'max' => 255]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'myaddress_id' => 'Myaddress ID',
+            'author' => 'Author',
+            'body' => 'Body',
+            'created_at' => 'Created At',
+        ];
+    }
+}
