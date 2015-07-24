92d04fb812ab7b8f518ed419e2079085b488cd7c
Aerica Maddatu <aericamaddatu417@gmail.com>
2015-07-24 12:06:24 +0800

    midterm_test/Maddatu_Aerica_midterms/ourspace/config/db.php

--- Practice/midterm_test/Maddatu_Aerica_midterms/ourspace/config/db.php
+++ Practice/midterm_test/Maddatu_Aerica_midterms/ourspace/config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=softdev_maddatu_aerica_ourspace',
     'username' => 'root',
-    'password' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 ];
