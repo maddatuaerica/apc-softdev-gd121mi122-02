f8c243a9a85c1988e0d9c5bfb650c1defac761e2
gaoaenj <jestine1795@gmail.com>
2015-09-01 18:34:38 +0800

    updated database

--- Database/arandiadb.sql
+++ Database/arandiadb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4.1
+-- version 4.0.9
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 14, 2015 at 07:14 AM
--- Server version: 5.5.32
--- PHP Version: 5.4.16
+-- Generation Time: Sep 01, 2015 at 12:34 PM
+-- Server version: 5.6.14
+-- PHP Version: 5.5.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `arandiadb`
 --
-CREATE DATABASE IF NOT EXISTS `arandiadb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `arandiadb`;
 
 -- --------------------------------------------------------
 
@@ -74,16 +72,16 @@ CREATE TABLE IF NOT EXISTS `register` (
   `birthplace` varchar(100) NOT NULL,
   `gender` tinyint(1) NOT NULL,
   `age` int(11) NOT NULL,
-  `contact_num` int(11) NOT NULL,
+  `contact_number` int(11) NOT NULL,
   `grade_level` varchar(45) NOT NULL,
-  `prev_school` varchar(100) NOT NULL,
+  `previous_school` varchar(100) NOT NULL,
   `father_name` varchar(50) NOT NULL,
   `father_occupation` varchar(50) NOT NULL,
   `mother_name` varchar(50) NOT NULL,
   `mother_occupation` varchar(50) NOT NULL,
-  `emerg_name` varchar(50) NOT NULL,
+  `emergency_contact` varchar(50) NOT NULL,
   `relation` varchar(45) NOT NULL,
-  `tel_num` int(11) NOT NULL,
+  `telephone_number` int(11) NOT NULL,
   `username` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
   `usertype` varchar(45) NOT NULL,
@@ -98,14 +96,21 @@ CREATE TABLE IF NOT EXISTS `register` (
 
 CREATE TABLE IF NOT EXISTS `student` (
   `Student_id` char(15) NOT NULL,
-  `Student_lname` varchar(45) NOT NULL,
-  `Student_fname` varchar(45) NOT NULL,
-  `Student_mname` varchar(45) NOT NULL,
+  `Student_lastname` varchar(45) NOT NULL,
+  `Student_firstname` varchar(45) NOT NULL,
+  `Student_middlename` varchar(45) NOT NULL,
   `Student_email` varchar(45) NOT NULL,
-  `Student_addr` varchar(100) NOT NULL,
-  `Student_cont` varchar(45) NOT NULL,
+  `Student_address` varchar(100) NOT NULL,
+  `Student_contact` varchar(45) NOT NULL,
   PRIMARY KEY (`Student_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`Student_id`, `Student_lastname`, `Student_firstname`, `Student_middlename`, `Student_email`, `Student_address`, `Student_contact`) VALUES
+('2011-100358', 'Mendoza', 'Aileen', 'Vista', 'avmendoza@arandia.edu.ph', 'Makati City', '1234567');
 
 -- --------------------------------------------------------
 
@@ -115,7 +120,7 @@ CREATE TABLE IF NOT EXISTS `student` (
 
 CREATE TABLE IF NOT EXISTS `subject` (
   `Subject_code` char(20) NOT NULL,
-  `Subject_desc` varchar(45) NOT NULL,
+  `Subject_description` varchar(45) NOT NULL,
   `Teacher_id` int(10) NOT NULL,
   PRIMARY KEY (`Subject_code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -128,13 +133,13 @@ CREATE TABLE IF NOT EXISTS `subject` (
 
 CREATE TABLE IF NOT EXISTS `teacher` (
   `Teacher_id` char(10) NOT NULL,
-  `Teacher_lname` varchar(45) NOT NULL,
-  `Teacher_fname` varchar(45) NOT NULL,
-  `Teacher_mname` varchar(45) NOT NULL,
+  `Teacher_lastname` varchar(45) NOT NULL,
+  `Teacher_firstname` varchar(45) NOT NULL,
+  `Teacher_middlename` varchar(45) NOT NULL,
   `Teacher_email` varchar(45) NOT NULL,
-  `Teacher_addr` varchar(100) NOT NULL,
-  `Teacher_cont` varchar(20) NOT NULL,
-  `Teacher_stat` varchar(10) NOT NULL,
+  `Teacher_address` varchar(100) NOT NULL,
+  `Teacher_contact` varchar(20) NOT NULL,
+  `Teacher_status` varchar(10) NOT NULL,
   PRIMARY KEY (`Teacher_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
