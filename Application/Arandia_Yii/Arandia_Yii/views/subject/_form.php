048dd02f594244e80455edd34f1fa435b054f5b8
gaoaenj <jestine1795@gmail.com>
2015-09-03 18:55:06 +0800

    created dropDownList in classes

--- Application/Arandia_Yii/Arandia_Yii/views/subject/_form.php
+++ Application/Arandia_Yii/Arandia_Yii/views/subject/_form.php
@@ -3,7 +3,11 @@
 use yii\helpers\Html;
 use yii\helpers\ArrayHelper;
 use yii\widgets\ActiveForm;
+<<<<<<< HEAD
 use app\models\Teacher;
+=======
+use app\models\teacher;
+>>>>>>> 5f97765ec078bc855f40e8322f0f3c01a356994b
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Subject */
@@ -19,8 +23,14 @@ use app\models\Teacher;
     <?= $form->field($model, 'subject_description')->textInput(['maxlength' => 45]) ?>
 
 	<?= $form->field($model, 'teacher_id')->dropDownList(
+<<<<<<< HEAD
 		ArrayHelper::map(Teacher::find()->all(),'teacher_id','teacher_lastname'),
 		['prompt'=>'Select Teacher']
+=======
+			ArrayHelper::map(Teacher::find()->all(),'teacher_id','teacher_lastname'),
+			['prompt'=>'Select Teacher']
+	
+>>>>>>> 5f97765ec078bc855f40e8322f0f3c01a356994b
 	)?>
 
     <div class="form-group">
