31e08b1818267d21cf0e12f03c9b99ae8cf30c56
Aileen Mendoza <mendozaaileen61@gmail.com>
2015-09-04 19:01:19 +0800

    edited index of frontend and backend

--- Application/Arandia_Yii/advanced/frontend/views/site/index.php
+++ Application/Arandia_Yii/advanced/frontend/views/site/index.php
@@ -7,45 +7,43 @@ $this->title = 'Arandia College';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+                <h2>Arandia College, Inc. is a leading private educational institution in southern Metro Manila with campuses in Parañaque City and Taguig City providing pre-school, elementary, high school, and tertiary education courses.</h2>
+			
     </div>
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Vision</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                <p>The Arandia College Inc., a private non-stock, nonprofit, and non- sectarian institution of learning envisions the full development of the learner through proper 
+                    instruction and training with complete physical facilities to produce individuals who are God-fearing, values-oriented, morally upright and nationalistic capable 
+                    of competing in the global revolution of knowledge which contributes largely in the progress of the community, society and country.</p>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Mission</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <h2>Mission:</h2>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+                <p>In pursuit of Vision, Arandia College Inc. commits to:
+                        <p>A.   Provide affordable quality education through effective instruction by competent teachers under the leadership of caring school officials.</p>
+                        <p>B.   Inculcate love of God, Country, Humanity and Nature.</p>
+                        <p>C.   Promote culture of competitiveness in all sectors of the academic community.</p>
+                        <p>D.   Develop young citizens' rights, duties and responsibilities for active participation in the progressive and productive home and community life.</p>
+                        <p>E.   Keep the learner abreast of technological, vocational and global trends and issues.</p>
+                        <p>F.   Enrich teachers; competencies in bringing out their best as educators.</p>
+                        <p>G.   Commit the school as leading institution in the society.</p>
+
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Offers:</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                 <p>Nursery - 3 years and 3 years old and a half by June of the academic year.</p>
+                <p>Kinder - 4 years and 4 years old and a half by June of the academic year.</p>
+                <p>Preparatory - 5 years and 5 years old and a half by June of the academic year.</p>
+                <p> Elementary</p>
+                <p>High School</p>
             </div>
         </div>
 
