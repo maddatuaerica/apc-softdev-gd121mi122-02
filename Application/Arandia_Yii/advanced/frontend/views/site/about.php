1bca8bfd1f05e87127b620fdd67d282fba5038f5
Aileen Mendoza <mendozaaileen61@gmail.com>
2015-09-04 19:30:14 +0800

    added header images for frontend about

--- Application/Arandia_Yii/advanced/frontend/views/site/about.php
+++ Application/Arandia_Yii/advanced/frontend/views/site/about.php
@@ -7,6 +7,108 @@ use yii\helpers\Html;
 $this->title = 'About';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+    <div class="site-index">
+
+<script type="text/javascript"> 
+
+var i = 0; 
+
+var image = new Array();   
+
+// LIST OF IMAGES 
+
+image[0] = "../web/images/building.jpg";
+
+image[1] =  "../web/images/facade2.jpg"; 
+
+image[2] = "../web/images/facade3.jpg";
+
+
+var k = image.length-1;    
+
+var caption = new Array(); 
+
+// LIST OF CAPTİONS  
+
+caption[0] = ""; 
+
+caption[1] = ""; 
+  
+function swapImage(){ 
+
+var el = document.getElementById("mydiv"); 
+
+el.innerHTML=caption[i]; 
+
+var img= document.getElementById("slide"); 
+
+img.src= image[i];  
+
+if(i < k ) { i++;}  
+
+else  { i = 0; } 
+
+setTimeout("swapImage()",3000);  
+
+} 
+
+function addLoadEvent(func) { 
+
+var oldonload = window.onload; 
+
+if (typeof window.onload != 'function') { 
+
+window.onload = func; 
+
+} else  { 
+
+window.onload = function() { 
+
+if (oldonload) { 
+
+oldonload(); 
+
+} 
+
+func(); 
+
+} 
+
+} 
+
+}  
+
+addLoadEvent(function() { 
+
+swapImage(); 
+
+});  
+
+</script> 
+
+<table style="border:3px solid #81DAF5;"> 
+
+<tr> 
+
+<td> 
+
+<img name="slide" id="slide" alt ="my images" height="285" width="1125" src="building.jpg"/> 
+
+</td> 
+
+</tr> 
+
+<tr> 
+
+<td align="center"style="font:small-caps bold 15px georgia; color: #81DAF5;"> 
+
+<div id ="mydiv"></div> 
+
+</tr> 
+
+</td> 
+
+</table> 
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
