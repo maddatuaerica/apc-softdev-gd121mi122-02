e27cb0cef027de2b614224117362f9b063803d62
maddatuaerica <aericamaddatu417@gmail.com>
2015-09-04 10:43:07 +0800

    crud classes in yii advanced backend

---
+++ Application/Arandia_Yii/advanced/backend/models/Classes.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "classes".
+ *
+ * @property integer $id
+ * @property string $section
+ * @property integer $student_id
+ * @property integer $subject_id
+ * @property integer $teacher_id
+ *
+ * @property Teacher $teacher
+ * @property Student $student
+ * @property Subject $subject
+ */
+class Classes extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'classes';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['section', 'student_id', 'subject_id', 'teacher_id'], 'required'],
+            [['student_id', 'subject_id', 'teacher_id'], 'integer'],
+            [['section'], 'string', 'max' => 45]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'section' => 'Section',
+            'student_id' => 'Student ID',
+            'subject_id' => 'Subject ID',
+            'teacher_id' => 'Teacher ID',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTeacher()
+    {
+        return $this->hasOne(Teacher::className(), ['id' => 'teacher_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getStudent()
+    {
+        return $this->hasOne(Student::className(), ['id' => 'student_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getSubject()
+    {
+        return $this->hasOne(Subject::className(), ['id' => 'subject_id']);
+    }
+}
