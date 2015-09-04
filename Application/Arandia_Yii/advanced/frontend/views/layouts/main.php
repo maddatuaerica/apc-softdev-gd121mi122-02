999b5f981a58aa914c311b828818e0bc24035d66
Aileen Mendoza <mendozaaileen61@gmail.com>
2015-09-04 10:28:06 +0800

    yii advanced

--- Application/Arandia_Yii/advanced/frontend/views/layouts/main.php
+++ Application/Arandia_Yii/advanced/frontend/views/layouts/main.php
@@ -25,12 +25,10 @@ AppAsset::register($this);
 <body>
 <?php $this->beginBody() ?>
 
-
-	
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'Arandia College',
+        'brandLabel' => 'My Company',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
@@ -69,7 +67,7 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; Arandia College <?= date('Y') ?></p>
+        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
 
         <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
