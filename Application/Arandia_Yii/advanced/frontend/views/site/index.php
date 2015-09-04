cba4b9ebd839d6af551bd08343f84fa7bccd9e12
gaoaenj <jestine1795@gmail.com>
2015-09-04 09:57:59 +0800

    created yii advanced

---
+++ Application/Arandia_Yii/advanced/frontend/views/site/index.php
@@ -0,0 +1,67 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        
+
+        <p class="lead">Arandia College, Inc. is a leading private educational institution in southern Metro Manila with campuses in Parañaque City and Taguig City providing pre-school, elementary, high school, and tertiary education courses.</p>
+
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Vision:</h2>
+
+                <p>The Arandia College Inc., a private non-stock, nonprofit, and non- sectarian institution of learning envisions the full development of the learner through proper instruction and training with complete physical facilities to produce individuals who are God-fearing, values-oriented, morally upright and nationalistic capable of competing in the global revolution of knowledge which contributes largely in the progress of the community, society and country.</p>
+
+               
+            </div>
+            <div class="col-lg-4">
+                <h2>Mission:</h2>
+
+                <p>In pursuit of Vision, Arandia College Inc. commits to: <br>
+
+				<li>Provide affordable quality education through effective instruction by competent teachers under the leadership of caring school officials.</li><br>
+
+				<li>Inculcate love of God, Country, Humanity and Nature.</li><br>
+
+				<li>Promote culture of competitiveness in all sectors of the academic community.</li><br>
+
+				<li>Develop young citizens' rights, duties and responsibilities for active participation in the progressive and productive home and community life.</li><br>
+
+				<li>Keep the learner abreast of technological, vocational and global trends and issues.</li><br>
+
+				<li>Enrich teachers; competencies in bringing out their best as educators.</li><br>
+
+				<li>Commit the school as leading institution in the society.</li><br>
+</p>
+
+
+            </div>
+            <div class="col-lg-4">
+                <h2>Offers:</h2>
+
+                <p><li>Nursery - 3 years and 3 years old and a half by June of the academic year.</li><br>
+
+				<li>Kinder - 4 years and 4 years old and a half by June of the academic year.</li><br>
+
+				<li>Preparatory - 5 years and 5 years old and a half by June of the academic year.</li><br>
+
+				<li>Elementary</li><br>
+
+				<li>High School</li><br>
+</p>
+
+              
+            </div>
+        </div>
+
+    </div>
+</div>
